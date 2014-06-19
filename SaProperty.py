fc5592e25f48537e1663d2877e03eb55e0b83cf4
sandy <sandeep_r80@yahoo.co.in>
2014-06-19 10:49:29 +0530

    Updated SaProperty for keys comparison

--- SaProperty.py
+++ SaProperty.py
@@ -3,24 +3,20 @@ import ConfigParser
 
 class SaProperty(object):
     
-    config = ConfigParser.ConfigParser()
-    
     def __init__(self, propertyFilePath):
+        self.config = ConfigParser.ConfigParser()
         self.propertyFilePath = propertyFilePath
-        self.config.read(propertyFilePath)
+        self.propertiesList=[]
         self._parsePropertyFile()
         
     def _parsePropertyFile(self):
         self.config.read(self.propertyFilePath)
         self.propertiesList = self.config.items("DEFAULT")
-        #print self.propertiesList
         self.propertiesDict=dict((self.property[0], self.property[1]) for self.property in self.propertiesList)
-        #print self.propertiesDict
         
-    def _getKeys(self):
+    def getKeys(self):
         return self.propertiesDict.keys()
-        
-            
+
 if __name__ == "__main__":
-    saProperty=SaProperty('/home/sandy/test.property')
-    print saProperty._getKeys()
+    saProperty1=SaProperty('/home/sandy/tmp.test.property')
+    print saProperty1.getKeys()
